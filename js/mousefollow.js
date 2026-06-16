da46280f9c826aa6dd889ff37f12cacc4b5e540d
Qwik <ytquer1x@gmail.com>
2026-06-16 22:14:02 +0200

    test

--- js/mousefollow.js
+++ js/mousefollow.js
@@ -5,7 +5,7 @@ const Cursor = document.getElementById("cursor");
 const HeropageBackground = document.getElementById("HeropageBackground");
 
 /*Vypnutí kurzoru a podsvícení na dotykových zařízení*/
-if (navigator.maxTouchPoints > 5) {
+if (navigator.maxTouchPoints > 1) {
     Cursor.style.display = "none"
     HeropageBackground.style.display = "none"
 }
